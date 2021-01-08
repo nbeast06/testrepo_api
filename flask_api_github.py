801a79afb971d3261dd7d1c345df40266b66074b
Nemi <49625726+nbeast06@users.noreply.github.com>
2021-01-08 11:37:01 +0800

    Update flask_api_github.py

--- flask_api_github.py
+++ flask_api_github.py
@@ -18,7 +18,7 @@ def api_root():
 def api_gh_message():
     if request.headers['Content-Type'] == 'application/json':
         my_info = json.dumps(request.json)
-        print(my_info)
+        pprint.pprint(my_info)
         return my_info
     
 if __name__ == '__main__':
